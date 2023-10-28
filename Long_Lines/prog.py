6773782162f0ed710f27ea1d8a0c333ddeb9a00d
Theodoros Koxanoglou <theokoxan@gmail.com>
2023-10-28 04:11:49 +0300

    first_Version

---
+++ Long_Lines/prog.py
@@ -0,0 +1,43 @@
+# calculate the height of the person
+def calculate_height_of_person(a, previous_height, c, q):
+    return (a * previous_height + c) % q
+
+# create a dictionary that stores the heights of the people
+heights = {}
+
+# compare heights of the people
+def noticable_people(heights):
+    noticable_people_number = 0
+    for i in range(1, len(heights)):
+        # if the height of the person is greater than the previous person
+        if heights[i] > heights[i - 1]:
+            noticable_people_number += 1
+        # if the height of the person is greater than all the other people in front of him
+        elif heights[i] > max(heights[:i]):
+            noticable_people_number += 1
+            
+    return noticable_people_number
+
+# main function
+if __name__=="__main__": 
+    print("Enter the values: ")
+
+    # store the five values from the terminal
+    input = [int(x) for x in input().split()]
+
+    print(input)
+
+    n = input[0]
+    first_person = input[1]
+    a = input[2]
+    c = input[3]
+    q = input[4]
+
+    for i in range(n):
+        if i == 0:
+            heights[i] = first_person
+        else:
+            heights[i] = calculate_height_of_person(a, heights[i], c, q)
+    
+    final_value = noticable_people(heights)
+    print(final_value)
