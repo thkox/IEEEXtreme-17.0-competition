2b5a523e270d09fbf92f2ead5757fd1c349f5714
Theodoros Koxanoglou <theokoxan@gmail.com>
2023-10-28 08:19:05 +0300

    last version

--- Caesar_Redux/prog.py
+++ Caesar_Redux/prog.py
@@ -2,9 +2,7 @@ def encrypt_message(message, key):
     message_ecrypted = []
     for i in range(len(message)):
         # ceaser cipher
-        ascii_message = [ord(char) for char in message[i]]
-        # change ascii value by key
-        ascii_message_2 = [ascii + key for ascii in ascii_message]
+        ascii_message = [(ord(char) - key - 97) % 26 + 97 for char in message[i]]
         # convert ascii message to text
         message_ecrypted.append("".join([chr(ascii) for ascii in ascii_message]))
     return " ".join(message_ecrypted)
@@ -14,18 +12,16 @@ def decrypt_message(message, key):
     message_decrypt = []
     for i in range(len(message)):
         # ceaser cipher
-        ascii_message = [ord(char) for char in message[i]]
-        # change ascii value by key
-        ascii_message = [ascii - key for ascii in ascii_message]
+        ascii_message = [(ord(char) - key + 97) % 26 + 97 for char in message[i]]
         # convert ascii message to text
         message_decrypt.append("".join([chr(ascii) for ascii in ascii_message]))
     return " ".join(message_decrypt)
 
 if __name__=="__main__": 
-    num_messages = int(input("How many messages do you want to encrypt/decrypt? "))
+    num_messages = int(input())
     for i in range(num_messages):
-        key = int(input("Enter the key: "))
-        message = input("Enter the message: ").split()
+        key = int(input())
+        message = input().split()
         if "the" in message:
             print(encrypt_message(message, key))
         else:
