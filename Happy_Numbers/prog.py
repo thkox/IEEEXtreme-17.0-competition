076f18dd4c953f68b36093e1349482382492cd25
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-10-28 06:04:18 +0300

    Happy_Numbers added.

---
+++ Happy_Numbers/prog.py
@@ -0,0 +1,44 @@
+# a simple parser for python. use get_number() and get_word() to read
+def parser():
+    while 1:
+        data = list(input().split(' '))
+        for number in data:
+            if len(number) > 0:
+                yield(number)   
+
+input_parser = parser()
+
+def get_word():
+    global input_parser
+    return next(input_parser)
+
+def get_number():
+    data = get_word()
+    try:
+        return int(data)
+    except ValueError:
+        return float(data)
+
+# numpy and scipy are available for use
+import numpy
+import scipy
+
+def is_Happy_num(n):
+  past = set()
+  while n != 1:
+        n = sum(int(i)**2 for i in str(n))
+        if n in past:
+            return False
+        past.add(n)
+  return True
+
+a = get_number()
+b = get_number()
+
+counter = 0
+
+for i in range(a, b+1):
+    if is_Happy_num(i):
+        counter += 1
+
+print(counter)
