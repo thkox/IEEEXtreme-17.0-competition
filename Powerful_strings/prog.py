01777d0b8801d2e76d4020fb7a3ad6f8eb4f8182
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-10-28 05:36:47 +0300

    powerful strings updated.

--- Powerful_strings/prog.py
+++ Powerful_strings/prog.py
@@ -23,7 +23,7 @@ def get_number():
 import numpy
 import scipy
 
-def findAllPossibleStrings(s, prefix, m, n, possibleStrings):
+def findAllPossibleStrings(chars, prefix, m, n, possibleStrings):
      
     # Base case: n is 0,
     # return prefix
@@ -37,28 +37,39 @@ def findAllPossibleStrings(s, prefix, m, n, possibleStrings):
     for i in range(m):
  
         # Next character of input added
-        newPrefix = prefix + s[i]
+        newPrefix = prefix + chars[i]
          
         # k is decreased, because 
         # we have added a new character
-        findAllPossibleStrings(s, newPrefix, m, n - 1, possibleStrings)
+        findAllPossibleStrings(chars, newPrefix, m, n - 1, possibleStrings)
 
 N = get_number()
 M = get_number()
 S = []
+
 possibleStrings = []
 
 for i in range(M):
     S.append(get_word())
 
-findAllPossibleStrings(S, "", M, N, possibleStrings)
+chars = set()
+for s in S:
+    chars = chars.union(set(s))
+chars = list(chars)
+print(*chars)
+
+findAllPossibleStrings(chars, "", M, N, possibleStrings)
+print(*possibleStrings)
 
 sum = 0
 
 for string in possibleStrings:
     x = 0
     for s in S:
+       # print(f'string: {string}, s: {s}, count: {string.count(s)}')
         x += string.count(s)
+        #print(f'x: {x}')
     sum += 2**x
+    #print(f'sum: {sum}')
         
 print(sum%998244353)
