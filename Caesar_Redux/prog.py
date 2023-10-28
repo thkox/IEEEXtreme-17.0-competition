781a9a84fc6f0b2de4d7fb546ab41c717d94382e
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-10-28 10:14:30 +0300

    ceasar redux updated.

--- Caesar_Redux/prog.py
+++ Caesar_Redux/prog.py
@@ -1,28 +1,66 @@
-def encrypt_message(message, key):
-    message_encrypt = []
-    for i in range(len(message)):
-        # Caesar cipher encryption
-        ascii_message = [(ord(char) - 97 - key) % 26 + 97 for char in message[i]]
-        # Convert ascii message to text
-        message_encrypt.append("".join([chr(ascii) for ascii in ascii_message]))
-    return " ".join(message_encrypt)
-    
-    
-def decrypt_message(message, key):
-    message_decrypt = []
-    for i in range(len(message)):
-        # Caesar cipher encryption
-        ascii_message = [(ord(char) - key + 97) % 26 + 97 for char in message[i]]
-        # convert ascii message to text
-        message_decrypt.append("".join([chr(ascii) for ascii in ascii_message]))
-    return " ".join(message_decrypt)
+# a simple parser for python. use get_number() and get_word() to read
+def parser():
+    while 1:
+        data = list(input().split(' '))
+        for number in data:
+            if len(number) > 0:
+                yield(number)   
 
-if __name__=="__main__": 
-    num_messages = int(input())
-    for i in range(num_messages):
-        key = int(input())
-        message = input().split()
-        if "the" in message:
-            print(encrypt_message(message, key))
+input_parser = parser()
+
+def get_word():
+    global input_parser
+    return next(input_parser)
+
+def get_number():
+    data = get_word()
+    try:
+        return int(data)
+    except ValueError:
+        return float(data)
+
+def encrypt_message(message, shift_val):
+    encrypted_message = ""
+    for char in message:
+        if char == " ":
+            encrypted_message += " "
+            continue
+        ord_char = ord(char)
+        if ord_char - shift_val < 97:
+            dif = shift_val - (ord_char - 97) 
+            new_ord_char = 122 - dif + 1
         else:
-            print(decrypt_message(message, key))
+            new_ord_char = ord_char - shift_val
+        encrypted_message += chr(new_ord_char)
+    print(encrypted_message)
+
+def decrypt_message(message, shift_val):
+    decrypted_mmessage = ""
+    for char in message:
+        if char == " ":
+            decrypted_mmessage += " "
+            continue
+        ord_char = ord(char)
+        if ord_char + shift_val > 122:
+            dif = shift_val - (122 - ord_char)
+            new_ord_char = 97 + dif - 1
+        else:
+            new_ord_char = ord_char + shift_val
+        decrypted_mmessage += chr(new_ord_char)
+    print(decrypted_mmessage)
+
+no_of_msgs = get_number()
+shift_vals = []
+messages = []
+
+for i in range(no_of_msgs):
+    shift_vals.append(get_number())
+    messages.append(input())
+
+for i in range(no_of_msgs):
+    shift_val = shift_vals[i]
+    message = messages[i]
+    if " the " in message:
+        encrypt_message(message, shift_val)
+    else:
+        decrypt_message(message, shift_val)
